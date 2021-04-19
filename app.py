cfea9580a539f60b2921593922dd78e6142ca866
David Smith <x300bps@icloud.com>
2021-04-18 21:02:37 -0400

    Still working. Linux seems to work.  Windows code still has problems.

--- app.py
+++ app.py
@@ -6,22 +6,154 @@
 # 4/11/21
 # License: MIT
 
+import datetime as dt
 import logging
 import os
+import socket
+import sys
+import traceback
+
+import ipxray.packet as ipxray
+
 
 
 # ** Module configuration constants **
 APP_NAME = "Hello Pi"
 APP_VERSION = "0.1"
 
-LOG_FILENAME = APP_NAME + ".log"
+LOG_FILENAME = APP_NAME.replace(" ", "_") + ".log"
 LOG_RECORD_FILTER_LEVEL = logging.INFO  # Log/propagate log records >= this level
+
+# The organizationally unique ids (OUI) for Raspberry Pis (first 3 bytes of the MAC address)
+# RASPBERRY_PI_MAC_OUIS = [bytes.fromhex("b827eb"), bytes.fromhex("dca632"), bytes.fromhex("e45f01")]
+RASPBERRY_PI_MAC_OUIS = [bytes.fromhex("5cf5da"), bytes.fromhex("347c25")]
+
+# Identify the current OS platform
+OS_PLATFORM = sys.platform
 
 
 # ** Module objects/variables **
 # Local logger for this module
 logger = logging.getLogger(__name__)
 
+# Command-line arguments
+cmdline_args_dict = {'-h': False, '-a': False, '-v': False}
+
+show_all_devices = False                    # When True, show all devices - not just RPIs
+verbose_output = False                      # When True, additional info is output
+windows_platform = "win32" in OS_PLATFORM   # True when on Windows; False otherwise
+
+
+
+def format_time(datetime: dt.datetime) -> str:
+    """
+    Apply simple formatting to display the time from a datetime.datetime object.
+    :param datetime: datetime.datetime object to use as time to display
+    :return: A time string formatted such as "1:27.03 AM"
+    """
+    if not isinstance(datetime, dt.datetime):
+        raise TypeError("TypeError: datetime.datetime object required.")
+
+    return datetime.strftime("%-I:%M.%S %p")
+
+
+def open_socket():
+    """
+    Open a raw socket in promiscuous mode to receive all LAN packets.
+    :return: A raw socket in promiscuous mode
+    """
+    # socket.
+    ETH_P_ALL = 0x0003      # Receive all layer 3 protocols
+    ETH_P_IP = 0x0800       # Receive only ip layer 3 protocol
+
+
+    if "win32" in OS_PLATFORM:
+        # Windows
+        print("Win")
+
+        # Create raw socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_IP)
+
+        # TODO: REMOVE DEBUG LINE AND FIND WHY CTRL-C DOESN'T TERMINATE
+        sock.settimeout(30)
+
+        # Bind raw socket to ip broadcast address
+        sock.bind(("255.255.255.255", 0))
+
+        # [Windows specific] Include IP headers
+        sock.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
+
+        # [Windows specific] receive all packages
+        sock.ioctl(socket.SIO_RCVALL, socket.RCVALL_ON)
+
+    elif "darwin" in OS_PLATFORM:
+        # Mac OSX
+        sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_IP))
+
+    else:
+        # *nix
+        sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_IP))
+
+    return sock
+
+
+def close_socket(sock: socket.socket):
+    if "win32" in OS_PLATFORM:
+        # Windows
+        # Take socket out of promisc mode
+        sock.ioctl(socket.SIO_RCVALL, socket.RCVALL_OFF)
+
+        # Shutdown and close the socket
+        # sock.shutdown(socket.SHUT_RDWR)
+        sock.close()
+
+    else:
+        # Shutdown and close the socket
+        sock.close()
+
+
+def show_hello(p, dhcp, is_rpi=True, verbose=False):
+    if is_rpi:
+        if verbose:
+            print(f"{format_time(dt.datetime.now())} | [IP Address {p.ip.format_ip_addr(dhcp.requested_ip)}] "
+                  f"- Raspberry Pi '{dhcp.hostname}' ({p.enet.format_mac_addr(p.enet.src_mac)}) said \"Hello!\"")
+
+        else:
+            print(f"{p.ip.format_ip_addr(dhcp.requested_ip)}: Raspberry Pi '{dhcp.hostname}' said \"Hello!\"")
+
+    else:
+        print(f"{p.ip.format_ip_addr(dhcp.requested_ip)}: Device '{dhcp.hostname}' said \"Hello!\"")
+
+
+def cmdline_options(argv: list, arg_dict: dict):
+    """
+    Parse command line arguments passed via 'argv'.
+    :param argv: The list of command-line arguments as produced by sys.argv
+    :param arg_dict: Dictionary of valid command-line argument entries of type {str: bool}.
+    :return: arg_dict with args specified in argv True and unspecified args False
+    """
+    if len(argv) > 1:
+        # Loop through options - skipping item 0 which is the name of the script
+        for arg in argv[1:]:
+            # Indicate which expected args that have been passed
+            if arg in arg_dict:
+                arg_dict[arg] = True
+
+    return arg_dict
+
+
+
+def show_help():
+    print(APP_NAME, f"(v{APP_VERSION:s})")
+    print("--------")
+    print("This utility watches the local area network (LAN) for DHCP requests and reports the ip address of any "
+          "Raspberry Pi that boots up connected to the same LAN. On Windows, option '-a' is always applied due to "
+          "the inability to access the raw ethernet frame."
+          )
+    print("OPTIONS:")
+    print("  -a\tDisplay ALL connected devices (not just RPis) that make a DHCP request for an ip address.")
+    print("  -h\tDisplay this help message.")
+    print("  -v\tDisplay verbose messages.")
 
 
 def main(argv=None):
@@ -30,16 +162,128 @@ def main(argv=None):
     :param argv: List of command-line arguments or None.
     :return: None
     """
-    # Initialize logging functionality
-    ver_str = f"(Version {APP_VERSION:s})"
-    logging.basicConfig(filename=os.path.join(os.path.dirname(os.path.abspath(__file__)), LOG_FILENAME), level=logging.INFO, format="%(asctime)s   [%(module)12.12s:%(lineno)4s] %(levelname)-8s %(message)s", filemode='w')
-    logging.info(APP_NAME + " " + ver_str)
+    global cmdline_args_dict
+    global show_all_devices
+    global verbose_output
 
-    print(APP_NAME)
-    print(ver_str+"\n")
+    try:
+        # Initialize logging functionality
+        ver_str = f"(Version {APP_VERSION:s})"
+        logging.basicConfig(filename=os.path.join(os.path.dirname(os.path.abspath(__file__)), LOG_FILENAME), level=logging.INFO, format="%(asctime)s   [%(module)12.12s:%(lineno)4s] %(levelname)-8s %(message)s", filemode='w')
+        logging.info(APP_NAME + " " + ver_str)
 
-    # Your Code Here
 
+        # Decipher command-line options and update
+        cmdline_args_dict = cmdline_options(argv, cmdline_args_dict)
+
+        # Cmdline arg: '-h' = Display help
+        if cmdline_args_dict.get('-h', False):
+            show_help()
+            sys.exit(0)
+
+        # Cmdline arg: '-a' = Display all device (not just RPis) DHCP requests
+        if cmdline_args_dict.get('-a', False):
+            show_all_devices = True
+
+        # Windows always applies '-a' option
+        elif windows_platform:
+            show_all_devices = True
+
+        # Cmdline arg: '-v' = Use verbose output statements
+        if cmdline_args_dict.get('-v', False):
+            verbose_output = True
+
+
+        # Print initial info
+        print(APP_NAME)
+        if windows_platform:
+            print("(Note: Windows always applies the '-a' option.)")
+
+        if cmdline_args_dict.get("-v", False):
+            # Verbose version
+            if show_all_devices:
+                msg = "Watching LAN for 'Hello' messages from all devices."
+            else:
+                msg = "Watching LAN for 'Hello' messages from Raspberry Pis."
+
+            msg += " Power up a connected Raspberry Pi to see its ip address."
+            print(msg)
+
+        else:
+            # Regular version
+            if show_all_devices:
+                print("Watching LAN for 'Hello' messages from all devices.")
+            else:
+                print("Watching LAN for 'Hello' messages from Raspberry Pis.")
+
+        print()
+
+
+        # Reuseable packet buffer
+        packet_buffer = bytearray(2*4096)
+
+        # Open the socket and look for desired packet
+        sock = open_socket()
+        try:
+            while True:
+                # Listen for packets
+                packet_size = sock.recv_into(packet_buffer)
+                if not packet_size:
+                    continue
+
+                # Attempt to decode the packet
+                if windows_platform:
+                    # Win sockets return ip protocol (layer 3)
+                    # NOTE: Without layer 2, we can't get the MAC address to test the OUI to see if device is a RPi.
+                    # The utility automatically applies the '-a' option to show all devices on Windows as a result.
+                    p = ipxray.INETPacket(ip_packet=packet_buffer)
+                    # $$$ TODO: Following line is debug
+                    if p.udp and not p.udp.dst_port == 22 and not p.udp.src_port == 22:
+                        print(p)
+
+                else:
+                    # *nix sockets return ethernet protocol (layer 2)
+                    p = ipxray.INETPacket(enet_packet=packet_buffer)
+
+                # Looking for DHCP messages being sent to DHCP server ...
+                if p.udp and p.udp.dst_port == 67:
+
+                    # Attempt to interpret DHCP packet
+                    dhcp = ipxray.DHCPPacket(p.udp.payload)
+                    if not dhcp.is_dhcp:
+                        continue
+
+                    # DHCP request message found
+                    if dhcp.message_type == dhcp.OptionMessageType.DHCPRequest:
+                        if windows_platform:
+                            # Windows must treat everything as not RPi since it doesn't have access to MAC address
+                            show_hello(p, dhcp, is_rpi=False, verbose=verbose_output)
+
+                        else:
+                            # If OUI is in RPi foundation OUIs, then it is a RPi
+                            is_rpi = p.enet.src_mac[0:3] in RASPBERRY_PI_MAC_OUIS
+
+                            if is_rpi or show_all_devices:
+                                show_hello(p, dhcp, is_rpi=is_rpi, verbose=verbose_output)
+
+        finally:
+            # Close the socket when exiting
+            if sock:
+                close_socket(sock)
+
+    except KeyboardInterrupt:
+        sys.exit(0)
+
+    except Exception as ex:
+        # TODO: Clean up this
+        print("Exception: ", ex)
+        print(traceback.format_exc())
+        logger.exception("Unhandled Exception:")
+        sys.exit(-1)
+
+    finally:
+        # Handle any shutdown cleanup
+        pass
 
 
 # Ensure that the software is run in the expected way - through the run.py script
---
+++ ipxray/packet.py
@@ -0,0 +1,388 @@
+# Package: ipxray
+# Package handles simple internet protocol dissection and parsing.
+#
+# David Smith
+# 4/15/21
+
+import enum
+from itertools import islice
+import struct
+from typing import Union, Optional
+
+
+
+# ** Layer 5 Protocols (Application Layer)
+
+class INETPacket:
+    """
+    Generic application layer protocol decoder. Decodes each of the layers in the stack.
+    """
+
+    class ProtocolLayer(enum.Enum):
+        ETHERNET = enum.auto()
+        IP = enum.auto()
+
+
+
+    def __init__(self, *,
+                 enet_packet: Optional[Union[bytes, bytearray]] = None,
+                 ip_packet: Optional[Union[bytes, bytearray]] = None):
+
+        if not any([enet_packet, ip_packet]):
+            raise ValueError("No packet parameter passed.")
+        elif not any([isinstance(enet_packet, (bytes, bytearray)), isinstance(ip_packet, (bytes, bytearray))]):
+            raise TypeError("Packet parameters must be of type 'bytes' or 'bytearray'.")
+
+
+        # Handles to the various layers
+        self.enet = None
+        self.ip = None
+
+        # One of the following layers will be populated (unless some other, less common layer 4 protocol is used)
+        self.icmp = None
+        self.udp = None
+        self.tcp = None
+
+        # Select parsing/decoding starting at the specified layer
+        if enet_packet:
+            # Use memoryview so data can be used in a zero-copy fashion
+            self.raw_packet = memoryview(enet_packet)
+
+            # Parse out the various components of the packet
+            self._parse(self.raw_packet, self.ProtocolLayer.ETHERNET)
+
+        elif ip_packet:
+            # Use memoryview so data can be used in a zero-copy fashion
+            self.raw_packet = memoryview(ip_packet)
+
+            # Parse out the various components of the packet
+            self._parse(self.raw_packet, self.ProtocolLayer.IP)
+
+
+    def _parse(self, raw_packet: Union[bytes, bytearray, memoryview], base_layer: ProtocolLayer):
+        """
+        Attempt the parse the packet one layer at a time starting at the ethernet layer (layer 2) up through the
+        tcp/udp/icmp (layer 4)
+        :param raw_packet: Raw ethernet or ip packet as specified in base_layer
+        :param base_layer: Enum ProtocolLayer specifying raw_packet lowest layer is ethernet (layer 2) or ip (layer 3)
+        :return:
+        """
+        # Parse as much of the packet as possible as long as there are no exceptions
+        try:
+
+            # Specify if packet data lowest level is ethernet or ip. If not ETHERNET (layer 2), assume IP (layer 3)
+            if base_layer == self.ProtocolLayer.ETHERNET:
+                # OSI Layer 2 (ethernet)
+                self.enet = ENETPacket(raw_packet)
+
+                # OSI Layer 3 (internet protocol)
+                self.ip = IPPacket(self.enet.payload)
+
+            elif base_layer == self.ProtocolLayer.IP:
+                # raw_packet base layer starts at the ip (layer 3) - doesn't contain ethernet (layer 2)
+                # OSI Layer 3 (internet protocol)
+                self.ip = IPPacket(raw_packet)
+
+            else:
+                raise ValueError("Invalid protocol layer specified")
+
+            # OSI Layer 4 (TCP, UDP, ICMP, or others)
+            if self.ip.protocol == "icmp":
+                self.icmp = ICMPPacket(self.ip.payload)
+            elif self.ip.protocol == "tcp":
+                self.tcp = TCPPacket(self.ip.payload)
+            elif self.ip.protocol == "udp":
+                self.udp = UDPPacket(self.ip.payload)
+
+        except Exception as ex:
+            pass
+
+
+    def __str__(self):
+        """
+        Return the string representation of the layers of the packet.
+        :return:
+        """
+        # Layer 2
+        val = f"{str(self.enet):s}\n" if self.enet else ""
+        # Layer 3
+        val += f"{str(self.ip):s}\n" if self.ip else ""
+        # Layer 4
+        if self.icmp:
+            val += f"{self.icmp}"
+        elif self.tcp:
+            val += f"{self.tcp}"
+        elif self.udp:
+            val += f"{self.udp}"
+
+        return val
+
+
+
+class DHCPPacket:
+    OPTIONS_OFFSET = 240
+
+
+    class Options(enum.IntEnum):
+        """
+        Option number assignments
+        """
+        DHCP_MESSAGETYPE = 53
+        SERVER_ID = 54
+        REQUEST_IPADDRESS = 50
+        HOSTNAME = 12
+
+
+
+    class OptionMessageType(enum.IntEnum):
+        """
+        Messagetype option fields
+        """
+        DHCPDiscover = 1
+        DHCPOffer = 2
+        DHCPRequest = 3
+        DHCPDecline = 4
+        DHCPAck = 5
+        DHCPNak = 6
+        DHCPRelease = 7
+        DHCPInform = 8
+
+
+    def __init__(self, udp_payload: Union[bytes, bytearray]):
+        self.is_dhcp = False
+        self.options = []
+        self.message_type = None
+        self.hostname = None
+        self.requested_ip = None
+        self.dhcpserver_ip = None
+
+        self._parse(udp_payload)
+
+
+    def _parse(self, udp_payload):
+        # Attempt to parse options fields
+        try:
+            # Create iterator at the beginning of options field
+            option_iter = islice(udp_payload, self.OPTIONS_OFFSET, None)
+
+            while True:
+                # DHCP option code
+                o_code = next(option_iter)
+
+                # Code of 0 signals empty option (padding)
+                if o_code == 0:
+                    continue
+                # Code of 255 signals end of options section
+                elif o_code == 255:
+                    raise StopIteration()
+
+                # Parse the option
+                o_len = next(option_iter)
+                o_data = [next(option_iter) for i in range(o_len)]
+                option = (o_code, o_len, bytes(o_data))
+
+                # Add to list of decoded options
+                self.options.append(option)
+
+                # Look for specific option codes
+                try:
+                    if o_code == self.Options.DHCP_MESSAGETYPE:
+                        # Indicate that this is truly a DHCP packet
+                        self.is_dhcp = True
+
+                        # Attempt to convert message type num to enum
+                        self.message_type = self.OptionMessageType(o_data[0])
+
+                    elif o_code == self.Options.REQUEST_IPADDRESS:
+                        self.requested_ip = bytes(o_data)
+
+                    elif o_code == self.Options.SERVER_ID:
+                        self.dhcpserver_ip = bytes(o_data)
+
+                    elif o_code == self.Options.HOSTNAME:
+                        # Convert source hostname to string
+                        self.hostname = bytes(o_data).decode(encoding="utf-8")
+
+                except (TypeError, ValueError):
+                    pass
+
+        except StopIteration:
+            pass
+        except Exception:
+            pass
+
+
+    def __str__(self):
+        mtype = self.message_type.name if self.message_type else "dhcp"
+        if self.is_dhcp:
+            return f"<{mtype}: option codes={[o[0] for o in self.options]}>"
+        else:
+            return f"<Not DHCP>"
+
+
+
+# ** Layer 4 Protocols (Transport Layer)
+
+class TCPPacket:
+
+    def __init__(self, tcp_packet: Union[bytes, bytearray]):
+        self.src_port = None
+        self.dst_port = None
+        self.seq_num = None
+        self.ack_num = None
+        self.payload = None
+
+        self._parse(tcp_packet)
+
+
+    def _parse(self, tcp_packet: Union[bytes, bytearray]):
+        self.src_port, self.dst_port, self.seq_num, self.ack_num = struct.unpack_from("!H H L L", tcp_packet, 0)
+
+        # Read data offset field and convert from num longs to num bytes
+        data_offset = 4 * (struct.unpack_from("!B", tcp_packet, 12)[0] >> 4)
+
+        self.payload = memoryview(tcp_packet)[data_offset:]
+
+
+    def __str__(self):
+        return (f"<tcp layer: src port={self.src_port}\t"
+               f"dst port={self.dst_port}>")
+
+
+
+class UDPPacket:
+
+    def __init__(self, udp_packet: Union[bytes, bytearray]):
+        self.src_port = None
+        self.dst_port = None
+        self.length = None
+        self.checksum = None
+        self.payload = None
+
+        self._parse(udp_packet)
+
+
+    def _parse(self, udp_packet: Union[bytes, bytearray]):
+        self.src_port, self.dst_port = struct.unpack_from("!H H", udp_packet, 0)
+        self.length, self.checksum = struct.unpack_from("!H H", udp_packet, 4)
+
+        self.payload = memoryview(udp_packet)[8:]
+
+
+    def __str__(self):
+        return (f"<udp layer: src port={self.src_port}\t"
+               f"dst port={self.dst_port}\tlength={self.length}>")
+
+
+
+class ICMPPacket:
+    # Convert ICMP type code to description
+    ICMP_TYPE = {0: "ping reply", 3: "dest unreachable", 4: "source quench", 5: "redirect", 8: "ping"}
+
+
+    def __init__(self, icmp_packet: Union[bytes, bytearray]):
+        self.icmp_type = None
+
+        self._parse(icmp_packet)
+
+
+    def _parse(self, ip_packet: Union[bytes, bytearray]):
+        # Decode ICMP type
+        self.icmp_type = struct.unpack_from("!B", ip_packet, 0)[0]
+        self.icmp_type = self.ICMP_TYPE.get(self.icmp_type, self.icmp_type)
+
+
+    def __str__(self):
+        return f"<icmp layer: type={self.icmp_type}>"
+
+
+
+# ** Layer 3 Protocols (Internet Layer)
+
+class IPPacket:
+    # Convert protocol field values to protocol names
+    PROTOCOL_ASSIGNMENTS = {1: "icmp", 6: "tcp", 17: "udp"}
+
+
+    def __init__(self, ip_packet: Union[bytes, bytearray]):
+        self.src_ip = None
+        self.dst_ip = None
+        self.protocol = None
+        self.payload = None
+
+        self._parse(ip_packet)
+
+
+    @staticmethod
+    def format_ip_addr(ip_addr: Union[bytes, bytearray]) -> str:
+        return ".".join((f"{b:d}" for b in ip_addr))
+
+
+    def _parse(self, ip_packet: Union[bytes, bytearray]):
+        # Decode IHL to get start of data location in packet
+        ip_header_length = struct.unpack_from("!B", ip_packet, 0)[0]
+        ip_header_length = (ip_header_length & 0x0F) * 4
+
+        # Total packet length including header
+        packet_length = struct.unpack_from("!H", ip_packet, 2)
+
+        # Identify the next-higher layer protocol
+        protocol = struct.unpack_from("!B", ip_packet, 9)[0]
+        self.protocol = self.PROTOCOL_ASSIGNMENTS.get(protocol, None)
+
+        # Ip addresses
+        header = struct.unpack_from("!4s 4s", ip_packet, 12)
+        self.src_ip = header[0]
+        self.dst_ip = header[1]
+
+        self.payload = memoryview(ip_packet)[ip_header_length:]
+
+
+    def __str__(self):
+        return (f"<ip layer: src ip={self.format_ip_addr(self.src_ip):s}\t"
+               f"dst ip={self.format_ip_addr(self.dst_ip):s}\tprotocol={self.protocol}>")
+
+
+
+# ** Layer 2 Protocols (Link Layer)
+
+class ENETPacket:
+
+    # Location of payload data in the packet
+    PAYLOAD_OFFSET = 14
+    CRC_CHECKSUM_LEN = 4
+
+    def __init__(self, enet_frame: Union[bytes, bytearray]):
+        self.src_mac = None
+        self.dst_mac = None
+        self.ether_type = None
+        self.crc_checksum = None
+        self.payload = None
+
+        self._parse(enet_frame)
+
+
+    @staticmethod
+    def format_mac_addr(mac_addr: Union[bytes, bytearray]) -> str:
+        return ":".join((f"{b:02x}" for b in mac_addr))
+
+
+    def _parse(self, enet_frame: Union[bytes, bytearray]):
+        # Decode ethernet header (dest mac, source mac, ether type)
+        header = struct.unpack_from("!6s 6s h", enet_frame, 0)
+
+        self.dst_mac = header[0]
+        self.src_mac = header[1]
+        self.ether_type = header[2]
+
+        # Last 4-bytes are crc-checksum
+        self.crc_checksum =  struct.unpack("!I", enet_frame[-4:])[0]
+
+        self.payload = memoryview(enet_frame)[self.PAYLOAD_OFFSET:-self.CRC_CHECKSUM_LEN]
+
+
+    def __str__(self):
+        return (f"<ethernet layer: src mac={self.format_mac_addr(self.src_mac):s}\t"
+                f"dst mac={self.format_mac_addr(self.dst_mac):s}>")
+
+
